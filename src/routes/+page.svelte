aa27922ef1b57ba4164d22466a16dcd952c831d2
Abel de Bruijn <abeldebruijn@hotmail.com>
2022-11-16 22:30:26 +0100

    threlte-5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,14 @@
 <script>
 	import { CircleGeometry, MeshStandardMaterial, BoxGeometry, DoubleSide } from 'three';
 	import { DEG2RAD } from 'three/src/math/MathUtils';
-	import {
-		AmbientLight,
-		Canvas,
-		Group,
-		Mesh,
-		OrbitControls,
-		PerspectiveCamera
-	} from '@threlte/core';
-	import { spring } from 'svelte/motion';
-	import SetRoot from './SetRoot.svelte';
+	import { Canvas, Group, OrbitControls, PerspectiveCamera, T } from '@threlte/core';
 
-	const scale = spring(1);
-
-	let flat = false;
-	let linear = false;
+	let flat = true;
+	let linear = true;
 </script>
 
 <div class="scene" style="height: 30rem; border: 1px solid black; border-radius: 1rem">
 	<Canvas {flat} {linear}>
-
 		<PerspectiveCamera position={{ x: 10, y: 10, z: 10 }} fov={24}>
 			<OrbitControls
 				maxPolarAngle={DEG2RAD * 80}
@@ -30,35 +18,28 @@
 			/>
 		</PerspectiveCamera>
 
-		<AmbientLight intensity={1} />
+		<T.AmbientLight intensity={1} />
 
 		<!-- Cube -->
-		<Group scale={$scale}>
-			<Mesh
-				interactive
-				on:pointerenter={() => ($scale = 2)}
-				on:pointerleave={() => ($scale = 1)}
-				position={{ x: 1, y: 0.5 }}
+		<Group>
+			<T.Mesh
+				position.x={1}
+				position.y={0.5}
 				castShadow
 				geometry={new BoxGeometry(1, 1, 1)}
 				material={new MeshStandardMaterial({ color: '#E15F55' })}
 			/>
 
-			<Mesh
-				interactive
-				on:pointerenter={() => ($scale = 2)}
-				on:pointerleave={() => ($scale = 1)}
-				position={{ x: -1, y: 0.5 }}
-				castShadow
-				geometry={new BoxGeometry(1, 1, 1)}
-				material={new MeshStandardMaterial({ color: '#308167' })}
-			/>
+			<T.Mesh position.x={-1} position.y={0.5}>
+				<T.BoxGeometry args={[1, 2, 1]} />
+				<T.MeshBasicMaterial color={'#308167'} />
+			</T.Mesh>
 		</Group>
 
 		<!-- Floor -->
-		<Mesh
+		<T.Mesh
 			receiveShadow
-			rotation={{ x: -90 * (Math.PI / 180) }}
+			rotation.x={-90 * (Math.PI / 180)}
 			geometry={new CircleGeometry(3, 72)}
 			material={new MeshStandardMaterial({ side: DoubleSide, color: '#ccc' })}
 		/>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
+
+import sequence from 'svelte-sequential-preprocessor';
 import preprocess from 'svelte-preprocess';
+import { preprocessThrelte } from '@threlte/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: sequence([preprocess(), preprocessThrelte()]),
 
 	ssr: {
 		noExternal: ['three', 'troika-three-text']
